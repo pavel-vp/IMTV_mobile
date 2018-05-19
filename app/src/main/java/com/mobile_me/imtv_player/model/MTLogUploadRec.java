6535ea5e34e613c434c85284af597146ebefe9c6
pavel-vp <pinegin.pavel@gmail.com>
2018-05-19 10:23:13 +0400

    Added new files

---
+++ app/src/main/java/com/mobile_me/imtv_player/model/MTLogUploadRec.java
@@ -0,0 +1,15 @@
+package com.mobile_me.imtv_player.model;
+
+import java.io.Serializable;
+
+public class MTLogUploadRec implements Serializable {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
